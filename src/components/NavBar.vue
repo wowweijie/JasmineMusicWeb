db1cadb44bab3152ef5485c63513b3fcaa7c802e
Wong Wei Jie <weighzhie@gmail.com>
2022-08-30 00:38:57 +0800

    nav bar sticky

---
+++ src/components/NavBar.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { debounce } from "lodash";
+import HelloWorld from "./HelloWorld.vue";
+
+const stickyNav = ref(false);
+
+function handleScroll(event) {
+  // Any code to be executed when the window is scrolled
+  // console.log("calling handleScroll");
+  if (window.scrollY > 75){
+    // console.log("sticky");
+    stickyNav.value = true;
+  }
+  else {
+    stickyNav.value = false;
+  }
+}
+
+onMounted(() => {
+  console.log(`App component is now mounted.`);
+  const handleDebouncedScroll = debounce(handleScroll, 100);
+  // window.addEventListener('scroll', handleDebouncedScroll);
+  window.addEventListener('scroll', handleScroll);
+});
+</script>
+
+<template>
+  <div :class="{ sticky :  stickyNav }">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+nav {
+  font-size: 18px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+div.sticky {
+  max-width: inherit;
+  width: 100%;
+  position: fixed;
+  top: 0
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 2rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
