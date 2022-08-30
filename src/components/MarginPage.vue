6ce825a41c1160bcc74df15623d7d409171fd4a0
Wong Wei Jie <weighzhie@gmail.com>
2022-08-30 23:35:09 +0800

    margin page

---
+++ src/components/MarginPage.vue
@@ -0,0 +1,26 @@
+<script setup> 
+import { reactive } from 'vue'
+
+const props = defineProps({
+  margin: {
+    type: Number,
+    default: 30,
+  },
+  fontSize: {
+    type: Number,
+    default: 18,
+  },
+});
+
+const styleObject = reactive({
+    margin: `0 ${props.margin}px`,
+    fontSize: `${props.fontSize} px`
+})
+
+console.log(styleObject)
+</script>
+<template>
+    <div :style="styleObject">
+        <slot></slot>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,77 @@
 <script setup>
 import TheWelcome from '@/components/TheWelcome.vue'
+import MarginPage from '@/components/MarginPage.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <MarginPage>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta non justo vitae efficitur. 
+    Ut vitae ultrices ipsum. Duis vitae ipsum metus. Aenean sagittis elit nibh, id congue neque ultrices in. 
+    Suspendisse vulputate auctor nisi non tincidunt. Duis mollis maximus neque, quis congue arcu blandit nec. 
+    Sed placerat malesuada malesuada. Integer nec neque id metus condimentum consequat. <br><br>
+
+    Praesent quis tincidunt odio. Sed pharetra nulla orci, id iaculis sapien luctus ac. 
+    Etiam vel ligula ac mi dignissim scelerisque. Morbi lacinia metus a massa feugiat tempus. 
+    Sed condimentum pretium egestas. Vivamus elit mi, tincidunt nec vestibulum quis, consectetur sed eros. 
+    Vivamus sed nisi vel velit rutrum ullamcorper quis sit amet tellus. Vestibulum maximus arcu a erat malesuada luctus. <br><br>
+
+    Etiam maximus elementum justo vitae cursus. Maecenas tempor diam vel sem fermentum facilisis. 
+    In eget massa pharetra, tempor turpis id, imperdiet dolor. Aliquam molestie at velit eget facilisis. 
+    Fusce venenatis ex sapien, vel consequat massa elementum sed. Donec ac lobortis nulla, venenatis scelerisque eros. 
+    Morbi congue urna quis orci tincidunt egestas. Nam dictum odio quis elit venenatis pellentesque non quis dolor. 
+    Pellentesque id justo convallis, feugiat erat non, rutrum massa. 
+    Nunc sit amet arcu imperdiet, luctus mauris a, luctus orci. Morbi facilisis varius aliquam.<br><br>
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta non justo vitae efficitur. 
+    Ut vitae ultrices ipsum. Duis vitae ipsum metus. Aenean sagittis elit nibh, id congue neque ultrices in. 
+    Suspendisse vulputate auctor nisi non tincidunt. Duis mollis maximus neque, quis congue arcu blandit nec. 
+    Sed placerat malesuada malesuada. Integer nec neque id metus condimentum consequat. <br><br>
+
+    Praesent quis tincidunt odio. Sed pharetra nulla orci, id iaculis sapien luctus ac. 
+    Etiam vel ligula ac mi dignissim scelerisque. Morbi lacinia metus a massa feugiat tempus. 
+    Sed condimentum pretium egestas. Vivamus elit mi, tincidunt nec vestibulum quis, consectetur sed eros. 
+    Vivamus sed nisi vel velit rutrum ullamcorper quis sit amet tellus. Vestibulum maximus arcu a erat malesuada luctus. <br><br>
+
+    Etiam maximus elementum justo vitae cursus. Maecenas tempor diam vel sem fermentum facilisis. 
+    In eget massa pharetra, tempor turpis id, imperdiet dolor. Aliquam molestie at velit eget facilisis. 
+    Fusce venenatis ex sapien, vel consequat massa elementum sed. Donec ac lobortis nulla, venenatis scelerisque eros. 
+    Morbi congue urna quis orci tincidunt egestas. Nam dictum odio quis elit venenatis pellentesque non quis dolor. 
+    Pellentesque id justo convallis, feugiat erat non, rutrum massa. 
+    Nunc sit amet arcu imperdiet, luctus mauris a, luctus orci. Morbi facilisis varius aliquam.<br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta non justo vitae efficitur. 
+    Ut vitae ultrices ipsum. Duis vitae ipsum metus. Aenean sagittis elit nibh, id congue neque ultrices in. 
+    Suspendisse vulputate auctor nisi non tincidunt. Duis mollis maximus neque, quis congue arcu blandit nec. 
+    Sed placerat malesuada malesuada. Integer nec neque id metus condimentum consequat. <br><br>
+
+    Praesent quis tincidunt odio. Sed pharetra nulla orci, id iaculis sapien luctus ac. 
+    Etiam vel ligula ac mi dignissim scelerisque. Morbi lacinia metus a massa feugiat tempus. 
+    Sed condimentum pretium egestas. Vivamus elit mi, tincidunt nec vestibulum quis, consectetur sed eros. 
+    Vivamus sed nisi vel velit rutrum ullamcorper quis sit amet tellus. Vestibulum maximus arcu a erat malesuada luctus. <br><br>
+
+    Etiam maximus elementum justo vitae cursus. Maecenas tempor diam vel sem fermentum facilisis. 
+    In eget massa pharetra, tempor turpis id, imperdiet dolor. Aliquam molestie at velit eget facilisis. 
+    Fusce venenatis ex sapien, vel consequat massa elementum sed. Donec ac lobortis nulla, venenatis scelerisque eros. 
+    Morbi congue urna quis orci tincidunt egestas. Nam dictum odio quis elit venenatis pellentesque non quis dolor. 
+    Pellentesque id justo convallis, feugiat erat non, rutrum massa. <br><br>
+
+    Nunc sit amet arcu imperdiet, luctus mauris a, luctus orci. Morbi facilisis varius aliquam.<br><br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta non justo vitae efficitur. 
+    Ut vitae ultrices ipsum. Duis vitae ipsum metus. Aenean sagittis elit nibh, id congue neque ultrices in. 
+    Suspendisse vulputate auctor nisi non tincidunt. Duis mollis maximus neque, quis congue arcu blandit nec. 
+    Sed placerat malesuada malesuada. Integer nec neque id metus condimentum consequat. <br><br>
+
+    Praesent quis tincidunt odio. Sed pharetra nulla orci, id iaculis sapien luctus ac. 
+    Etiam vel ligula ac mi dignissim scelerisque. Morbi lacinia metus a massa feugiat tempus. 
+    Sed condimentum pretium egestas. Vivamus elit mi, tincidunt nec vestibulum quis, consectetur sed eros. 
+    Vivamus sed nisi vel velit rutrum ullamcorper quis sit amet tellus. Vestibulum maximus arcu a erat malesuada luctus. <br><br>
+
+    Etiam maximus elementum justo vitae cursus. Maecenas tempor diam vel sem fermentum facilisis. 
+    In eget massa pharetra, tempor turpis id, imperdiet dolor. Aliquam molestie at velit eget facilisis. 
+    Fusce venenatis ex sapien, vel consequat massa elementum sed. Donec ac lobortis nulla, venenatis scelerisque eros. 
+    Morbi congue urna quis orci tincidunt egestas. Nam dictum odio quis elit venenatis pellentesque non quis dolor. 
+    Pellentesque id justo convallis, feugiat erat non, rutrum massa. 
+    Nunc sit amet arcu imperdiet, luctus mauris a, luctus orci. Morbi facilisis varius aliquam.<br><br>
+  </MarginPage>
   </main>
 </template>
