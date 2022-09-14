5d9beb84317f9607b3e7d10f86abbd5f00f55d54
Wong Wei Jie <weighzhie@gmail.com>
2022-09-14 22:56:15 +0800

    image box with caption

--- src/components/ImageBox.vue
+++ src/components/ImageBox.vue
@@ -1,71 +1,92 @@
 <script>
-import TheWelcome from '@/components/TheWelcome.vue'
-import MarginPage from '@/components/MarginPage.vue'
-import VueEasyLightbox from 'vue-easy-lightbox'
-import { ref, defineComponent } from 'vue'
+import TheWelcome from "@/components/TheWelcome.vue";
+import MarginPage from "@/components/MarginPage.vue";
+import VueEasyLightbox from "vue-easy-lightbox";
+import { ref, defineComponent } from "vue";
 
 export default {
-  components:{
+  components: {
     VueEasyLightbox,
-    MarginPage
+    MarginPage,
   },
   props: {
-    imgList: Array
+    imgList: Array,
+    caption: String
   },
   setup(props) {
-    const visibleRef = ref(false)
-    const indexRef = ref(0) // default 0
-    const imgsRef = ref([])
+    const visibleRef = ref(false);
+    const indexRef = ref(0); // default 0
+    const imgsRef = ref([]);
     // Img Url , string or Array of string
     // ImgObj { src: '', title: '', alt: '' }
     // 'src' is required
     // allow mixing
-
+    
     const onShow = () => {
-      visibleRef.value = true
-    }
+      visibleRef.value = true;
+    };
     const showSingle = () => {
-      imgsRef.value = "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg"
+      imgsRef.value =
+        "https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg";
       // or
       // imgsRef.value  = {
       //   title: 'this is a placeholder',
       //   src: 'http://via.placeholder.com/350x150'
       // }
-      onShow()
-    }
+      onShow();
+    };
     const showMultiple = () => {
-      imgsRef.value = props.imgList
+      imgsRef.value = props.imgList;
       // or
       // imgsRef.value = [
       //   { title: 'test img', src: 'http://via.placeholder.com/350x150' },
       //   'http://via.placeholder.com/350x150'
       // ]
-      indexRef.value = 0 // index of imgList
-      onShow()
-    }
-    const onHide = () => (visibleRef.value = false)
+      indexRef.value = 0; // index of imgList
+      onShow();
+    };
+    const onHide = () => (visibleRef.value = false);
+
+    const displayImgRef = ref(props.imgList[0]);
 
     return {
+      displayImgRef,
       visibleRef,
       indexRef,
       imgsRef,
-      showSingle,
       showMultiple,
-      onHide
-    }
-  }
-}
+      onHide,
+    };
+  },
+};
 </script>
 
 <template>
-    <button @click="showMultiple">Show a group of pictures.</button>
-
-  <vue-easy-lightbox
+  <div class="img-frame">
+    <img @click="showMultiple" :src="displayImgRef" width="200" height="200" />
+    <p>
+      {{ caption }}
+    </p>
+    <vue-easy-lightbox
       :visible="visibleRef"
       :imgs="imgsRef"
       :index="indexRef"
-      :moveDisabled=true
-      scrollDisabled=true
+      :moveDisabled="true"
+      :scrollDisabled="true"
       @hide="onHide"
     ></vue-easy-lightbox>
+  </div>
 </template>
+
+<style scoped>
+img {
+  max-width: 100%;
+  max-height: 100%;
+  border: 6px solid #ffff;
+  margin: 3px 10px 6px;
+}
+
+div.img-frame{
+  text-align: center;
+}
+</style>
--- src/views/GalleryView.vue
+++ src/views/GalleryView.vue
@@ -14,12 +14,47 @@ export default {
 <template>
   <main>
     <MarginPage>
-      <ImageBox
-        :imgList="[
-          'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
-          'https://drive.google.com/uc?id=193y4-7w53sQovSd8lPT850VcU6xABV6m'
-        ]"
-      />
+      <gallery>
+        <ImageBox
+          :imgList="[
+            'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
+            'https://drive.google.com/uc?id=193y4-7w53sQovSd8lPT850VcU6xABV6m',
+          ]"
+          caption="HIHIH"
+        />
+        <ImageBox
+          :imgList="[
+            'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
+            'https://drive.google.com/uc?id=193y4-7w53sQovSd8lPT850VcU6xABV6m',
+          ]"
+          caption="HIHIH"
+        />
+        <ImageBox
+          :imgList="[
+            'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
+            'https://drive.google.com/uc?id=193y4-7w53sQovSd8lPT850VcU6xABV6m',
+          ]"
+          caption="HIHIH"
+        />
+        <ImageBox
+          :imgList="[
+            'https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg',
+            'https://drive.google.com/uc?id=193y4-7w53sQovSd8lPT850VcU6xABV6m',
+          ]"
+          caption="HIHIH"
+        />
+      </gallery>
+      <br />
+      <br />
     </MarginPage>
   </main>
 </template>
+
+<style scoped>
+gallery {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  row-gap: 30px;
+}
+</style>
